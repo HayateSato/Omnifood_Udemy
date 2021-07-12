f73cbdefac8f5b63b556aa203ab81fb50686fb35
hayate <hayatesato1995@gmail.com>
2021-07-12 21:41:35 +0900

    styling sticky nav with waypoit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 
 
-$(document).ready(function() {
+
+
+
+    // var waypoints = $('#handler-first').waypoint(function(direction) {
+    //     notify(this.element.id + ' hit 25% from top of window') 
+    //   }, {
+    //     offset: '25%'
+    //   })
+
+
+
+
+
     
     // this was just a test to see it jQuery is working
 //     $('h1').click(function() {
